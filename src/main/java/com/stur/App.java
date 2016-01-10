692c1cddaa5d474719d28cb5fb259e240250869d
stur <sdongjie@126.com>
2016-01-10 23:24:21 +0800

    add littleproxy

--- src/main/java/com/stur/App.java
+++ src/main/java/com/stur/App.java
@@ -1,13 +1,39 @@
 package com.stur;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import com.sun.deploy.net.HttpRequest;
+import com.sun.deploy.net.HttpResponse;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.HttpObject;
+import org.littleshoot.proxy.HttpFilters;
+import org.littleshoot.proxy.HttpFiltersAdapter;
+import org.littleshoot.proxy.HttpFiltersSourceAdapter;
+import org.littleshoot.proxy.HttpProxyServer;
+import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        HttpProxyServer server =
+                DefaultHttpProxyServer.bootstrap()
+                        .withPort(8080)
+                        .withFiltersSource(new HttpFiltersSourceAdapter() {
+                            public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
+                                return new HttpFiltersAdapter(originalRequest) {
+                                    @Override
+                                    public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+                                        // TODO: implement your filtering here
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public HttpObject serverToProxyResponse(HttpObject httpObject) {
+                                        // TODO: implement your filtering here
+                                        return httpObject;
+                                    }
+                                }
+                            }).start();
+                        }
     }
-}
